ca9ffeec350e555a199110cb120faee6d44bc4ad
Fuyuki Malahom <fuyuki325@gmail.com>
2024-07-02 15:09:05 -0700

    Initialized upload button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      'times': ["Times New Roman"],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",

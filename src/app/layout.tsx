46071a154acf879d51ecf7b59326944b852d42cf
Fuyuki Malahom <fuyuki325@gmail.com>
2024-07-03 13:13:48 -0700

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <body className={inter.className}>{children}</body>
     </html>
   );
